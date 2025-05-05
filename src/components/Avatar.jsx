209fe7296ab24f330d268f076acf217d5484c773
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-05-06 04:17:28 +0600

    1.11

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,9 +1,9 @@
 export default function Avatar({ age }) {
-    return (
-        <div>
-            <div>
-                <h1>Profile</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <h1>Profile</h1>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/New.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const New = () => {
+  return (
+    <div>
+      <ul>
+        <li>Creola Katherine Johnson: mathematician</li>
+        <li>Mario José Molina-Pasquel Henríquez: chemist</li>
+        <li>Mohammad Abdus Salam: physicist</li>
+        <li>Percy Lavon Julian: chemist</li>
+        <li>Subrahmanyan Chandrasekhar: astrophysicist</li>
+      </ul>
+    </div>
+  );
+};
+
+export default New;
