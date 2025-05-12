3f2567482448f6b105cd342bea37f7f90cf052e4
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-05-12 19:41:32 +0600

    M

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,31 @@
 import { useState } from "react";
 
-export default function MovingDot() {
-  const [position, setPosition] = useState({
-    x: 0,
-    y: 0,
-  });
+let initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
+
+export default function List() {
+  const [artists, setArtists] = useState(initialArtists);
+
   return (
-    <div
-      onPointerMove={(e) => {
-        setPosition({
-          x: e.clientX,
-          y: e.clientY,
-        });
-      }}
-      style={{
-        position: "relative",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          backgroundColor: "red",
-          borderRadius: "50%",
-          transform: `translate(${position.x}px, ${position.y}px)`,
-          left: -10,
-          top: -10,
-          width: 20,
-          height: 20,
-        }}
-      />
-    </div>
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>
+            {artist.name}{" "}
+            <button
+              onClick={() => {
+                setArtists(artists.filter((a) => a.id !== artist.id));
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
