2d48cc5b157e8e25dabcbd56a4b88ad0f3f9b5ce
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-05-23 03:14:17 +0600

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import Form from "./components/Form";
 import "./styles.css";
 
-const statuses = ["empty", "typing", "submitting", "success", "error"];
-
 function App() {
-    return (
-        <div>
-            {statuses.map((status) => (
-                <Form key={status} status={status} />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
